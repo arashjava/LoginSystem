250e8a17d660f2b62e4554ddfbfe5509f44c1d5c
root <shadmaniarash@gmail.com>
2017-12-19 12:29:20 -0500

    first commit

---
+++ home.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+	<link rel='stylesheet' href='page_css.css'>
+	<title> Project Share</title>
+</head>
+<body>
+		<table cellpadding='3' cellspacing='3' class='tab_main'>
+
+			<!--Nav_Tabs-->
+			<tr align='center' bgcolor='lightgrey' class='td_bor'>
+				<td width='5%'> <a href='home.php'> Home </a></td>
+				<td width='5%'> <a href='login.php'>Login </a></td>
+				<td width='5%'> <a href='secure_signup.php'>Sign-up </a></td> 
+			</tr>
+			
+			<tr>
+				<td> <hr> </td> 
+				<td> <hr> </td> 
+				<td> <hr> </td> 
+			</tr>
+			
+			<tr>
+				<td colspan='4'> 
+					<p> 
+						  <h3>Welcome to our Home Page</h3>
+					</p>
+	
+				</td>
+			</tr>
+		</table>
+		<footer align='center'>
+			&copy; All Rights Reserved.	
+		</footer>
+</body>
+</html>
